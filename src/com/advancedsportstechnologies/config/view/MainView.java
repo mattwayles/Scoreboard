97569104ba417e7b1363653cf98ace6667ba7f09
mattwayles <mattwayles@gmail.com>
2018-09-25 19:54:33 -0400

    v0.4 ===BEGIN===
    Goals for this version:
    
    *Embedded fonts
    *Pi 4:3 display optimization
    *Aesthetic changes
    *Easter egg incorporation on Pi

--- src/com/advancedsportstechnologies/config/view/MainView.java
+++ src/com/advancedsportstechnologies/config/view/MainView.java
@@ -23,7 +23,7 @@ import java.util.Set;
 
 public class MainView extends Node {
     private static final String TITLE = "Chucktown Social Scoreboard";
-    private static final String VERSION = "v0.3";
+    private static final String VERSION = "v0.4";
 
     private Pane mainView;
     private Node currentControl;
